0acc2122aaaed7b9d080afb86c4bb8e96499c876
bergold <emil.bergold@outlook.com>
2014-10-09 13:42:32 +0200

    fix test

--- test/util.js
+++ test/util.js
@@ -1,6 +1,7 @@
 var assert = require("assert");
+
 describe("Util", function() {
-  if("should be consistent bool values", function() {
+  it("should be consistent bool values", function() {
     assert.equal(true, true);
     assert.equal(false, false);
   });
